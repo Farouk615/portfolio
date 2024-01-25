21e14520f05abfbe86c7c8123c9c5fad355f38ad
lenovo <faroukayadi1@gmail.com>
2024-01-25 11:16:56 +0100

    placing body background color

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@ let checkbox = document.getElementById("theme-switch");
 let topHeader = document.querySelector(".top-header")
 let bulbLightMode = document.querySelector("#bulb-light-mode")
 let bulbDarkMode = document.querySelector("#bulb-dark-mode")
+let headerElement = document.querySelector("header h1");
 
 let divs = document.querySelectorAll('[data-theme]')
 // Add an event listener to the checkbox
-bulbDarkMode.addEventListener("click", () => {
-    // Toggle the data attribute based on the checkbox state
+bulbDarkMode.addEventListener("click", toggleTheme);
+bulbLightMode.addEventListener("click",toggleTheme)
+function toggleTheme(){
     document.body.classList.toggle("dark-mode")
     topHeader.classList.toggle("light-mode-box-shadow")
-    bulbDarkMode.classList.toggle("invisible")
-    bulbLightMode.classList.toggle("invisible")
-});
+    bulbDarkMode.classList.toggle("d-none")
+    bulbLightMode.classList.toggle("d-none")
+    headerElement.classList.toggle("purple-color")
+}
