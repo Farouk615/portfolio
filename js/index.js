c63ce7ae0cc41597f2007979737b4dc551b81833
mohamed farouk <fa@knowlepsy>
2024-02-16 00:37:40 +0100

    adding contact me section

--- js/index.js
+++ js/index.js
@@ -8,69 +8,93 @@ let resumeContainer = document.getElementById('resume')
 let leftArrows = document.querySelectorAll('.left-arrow-local')
 let rightArrows = document.querySelectorAll('.right-arrow-local')
 let carHolder = document.querySelector('.car-holder')
-
+let experiences = document.querySelectorAll('.experience')
+let flags = document.querySelectorAll('.flag')
+let emailForm = document.querySelector('.email-form ')
+experiences[0].classList.add('explode-animation')
 
 let welcomeText = "\"Hello there !\"";
+let experienceCounter = 0;
 
-let divs = document.querySelectorAll('[data-theme]')
 // Add an event listener to the checkbox
 bulbDarkMode.addEventListener("click", toggleTheme);
-bulbLightMode.addEventListener("click",toggleTheme)
-function toggleTheme(){
+bulbLightMode.addEventListener("click", toggleTheme)
+
+function toggleTheme() {
     document.body.classList.toggle("dark-mode")
     document.body.classList.toggle("light-mode")
     topHeader.classList.toggle("light-mode-box-shadow")
-    elementsToggle.forEach((element)=>element.classList.toggle("d-none"))
+    elementsToggle.forEach((element) => element.classList.toggle("d-none"))
     resumeContainer.classList.toggle("white-background")
     headerElement.classList.toggle("purple-color")
-    listElements.forEach((element)=>element.classList.toggle("purple-color"))
+    listElements.forEach((element) => element.classList.toggle("purple-color"))
+    emailForm.classList.toggle('background-light')
+    emailForm.classList.toggle('box-shadow-light')
 }
 
-function showText(target, message,index, interval) {
+function showText(target, message, index, interval) {
     if (index < message.length) {
         // Append the next letter to the element
         document.getElementById(target).innerHTML += message[index++];
         // Call the function again after a delay
-        setTimeout(function() {
+        setTimeout(function () {
             showText(target, message, index, interval);
         }, interval);
     } else {
         // When the message is complete, call another function to remove it
-        hideText(target, message, message.length-1, interval);
+        hideText(target, message, message.length - 1, interval);
     }
 }
 
 // A function that removes one letter at a time from a text element
-function hideText(target, message,index, interval) {
+function hideText(target, message, index, interval) {
     if (index > 0) {
         // Remove the last letter from the element
         document.getElementById(target).innerHTML = message.substring(0, index--);
         // Call the function again after a delay
-        setTimeout(function() {
+        setTimeout(function () {
             hideText(target, message, index, interval);
         }, interval);
-    }else{
-        showText("welcome-text", welcomeText,0, 100);
+    } else {
+        showText("welcome-text", welcomeText, 0, 100);
     }
 }
-showText("welcome-text", welcomeText,0, 100);
 
-leftArrows.forEach((element)=>{
-    element.addEventListener("click",carClickLeft)
+showText("welcome-text", welcomeText, 0, 100);
+
+leftArrows.forEach((element) => {
+    element.addEventListener("click", carClickLeft)
 })
-rightArrows.forEach((element)=>{
-    element.addEventListener("click",carClickRight)
+rightArrows.forEach((element) => {
+    element.addEventListener("click", carClickRight)
 })
 
-function carClickLeft(){
+function carClickLeft() {
+    experiences[experienceCounter].classList.remove('explode-animation')
+    experienceCounter++;
     let currentPos = carHolder.style.right || 0;
-    if(parseInt(currentPos, 10)<90)
-    carHolder.style.right=`${parseInt(currentPos, 10) + 30}%`;
+    if (parseInt(currentPos, 10) < 90)
+        carHolder.style.right = `${parseInt(currentPos, 10) + 32}%`;
+    if(experienceCounter===3)
+        flags.forEach((element)=>element.classList.add('margin-bottom'))
+    else{
+        flags.forEach((element)=>element.classList.remove('margin-bottom'))
+    }
+    experiences[experienceCounter].classList.add('explode-animation')
 }
 
-function carClickRight(){
+function carClickRight() {
+    experiences[experienceCounter].classList.remove('explode-animation')
+    experienceCounter--;
     const currentPos = carHolder.style.right || 0;
-    if(parseInt(currentPos, 10)>2){
-        carHolder.style.right=`${parseInt(currentPos, 10) - 30}%`;
+    if (parseInt(currentPos, 10) > 2) {
+        carHolder.style.right = `${parseInt(currentPos, 10) - 32}%`;
     }
+    if(experienceCounter===3)
+        flags.forEach((element)=>element.classList.add('margin-bottom'))
+    else{
+        flags.forEach((element)=>element.classList.remove('margin-bottom'))
+    }
+    experiences[experienceCounter].classList.add('explode-animation')
 }
+
